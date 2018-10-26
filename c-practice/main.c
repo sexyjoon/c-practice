5b2210a06a71ca9393b4e55fd2fadd6a3b6d9095
sexyjoon <answns0486@gmail.com>
2018-10-27 00:20:01 +0900

    Initial Commit

---
+++ c-practice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-practice
+//
+//  Created by JoonYoung on 27/10/2018.
+//  Copyright © 2018 sexyjoon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
